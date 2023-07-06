ea123214586ed78574ae98fe7a4bbd6b1636e4f2
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-07-06 21:32:36 +0100

    Update all files
    Removed the prompts/confirms and add a form instead as this is what the project required
    Added splice so that once a book is removed from the library it is also removed from the DOM completely
    Added margin to the form submit button and the created book divs

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-let myBooks = [];
+let myBooks = []
 
 window.onload = function () {
-    const newBookBtn = document.getElementById('newBookBtn')
-    newBookBtn.addEventListener('click', addBook)
-}
+    const newBookForm = document.getElementById('newBookForm')
+    newBookForm.addEventListener('submit', addBook)
+};
 
 
-function addBook() {
-    const title = prompt("What book would you like to add?:")
-    const author = prompt("Who is the author of this book?:")
-    const pages = parseInt(prompt("How many pages does this book have?:"))
-    const read = confirm("Have you read this book? Ok for Yes, Cancel for No:")
+function addBook(event) {
+    event.preventDefault()
+
+    const titleInput = document.getElementById('title')
+    const authorInput = document.getElementById('author')
+    const pagesInput = document.getElementById('pages')
+    const readInput = document.getElementById('read')
+
+    const title = titleInput.value
+    const author = authorInput.value
+    const pages = parseInt(pagesInput.value)
+    const read = readInput.checked
 
     const book = {
         title: title,
@@ -19,21 +26,28 @@ function addBook() {
         read: read
     };
 
-    myBooks.push(book);
+    myBooks.push(book)
 
-    addBookToLibrary();
+    addBookToLibrary()
+
+    titleInput.value = ''
+    authorInput.value = ''
+    pagesInput.value = ''
+    readInput.checked = false
 }
 
 
 function addBookToLibrary() {
     const bookDisplay = document.getElementById('book-display')
-    bookDisplay.innerHTML = '';
+    bookDisplay.innerHTML = ''
 
     for (let i = 0; i < myBooks.length; i++) {
-        const book = myBooks[i];
+        const book = myBooks[i]
 
         const bookDiv = document.createElement('div')
         bookDiv.classList.add('book')
+
+        bookDiv.style.margin = '5px 10px'
 
         const title = document.createElement('p')
         title.textContent = `Title: ${book.title}`
@@ -53,7 +67,6 @@ function addBookToLibrary() {
         const nowRead = document.createElement('button')
         nowRead.textContent = `Read`
 
-
         bookDiv.appendChild(title)
         bookDiv.appendChild(author)
         bookDiv.appendChild(pages)
@@ -66,19 +79,20 @@ function addBookToLibrary() {
         }
 
         nowRead.addEventListener('click', function () {
-            const readButton = this;
-            readButton.parentNode.removeChild(readButton);
+            const readButton = this
+            readButton.parentNode.removeChild(readButton)
             read.textContent = 'Read: Yes'
-        })
+        });
 
-        bookDiv.appendChild(remove)
+        bookDiv.appendChild(remove);
 
-        remove.style.height = "40px"
+        remove.style.height = '40px'
 
         bookDisplay.appendChild(bookDiv)
 
         remove.addEventListener('click', function () {
+            myBooks.splice(i, 1)
             bookDisplay.removeChild(bookDiv)
-        })
+        });
     }
 }
