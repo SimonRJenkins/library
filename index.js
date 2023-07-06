cd7486e8b87080303a9a1f871ee5be30172415d6
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-07-06 19:27:58 +0100

    Update index.js & index.html & index.css
    Refactored the user required information
    Removed The Hobbit example
    Changed p element to a div to display books that have been added
    Added a for loop so that a div is created for each book added
    Added p element for title, author, pages and read yes/no
    Added appendChild for the information to be added to a div

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-bookDisplay = document.getElementById("book-display")
-newBook = document.getElementById("new-book")
-
 let myBooks = [];
-bookDisplay.innerHTML = [myBooks];
 
 
-newBook.addEventListener("click", function () {
-    var title = prompt("What book would you like to add?")
-    var author = prompt("Who is the author of this book?")
-    var pages = prompt("How many pages does this book have?")
-    var read = prompt("Have you read this book?")
-    myBooks.push(title, author, pages + " pages", read);
-    bookDisplay.innerHTML = [myBooks];
-})
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-        console.log(title, author, pages, read)
+
+function addBook() {
+    const title = prompt("What book would you like to add?:")
+    const author = prompt("Who is the author of this book?:")
+    const pages = parseInt(prompt("How many pages does this book have?:"))
+    const read = confirm("Have you read this book? Ok for Yes, Cancel for No:")
+
+    const book = {
+        title: title,
+        author: author,
+        pages: pages,
+        read: read
+    };
+
+    myBooks.push(book);
+
+    addBookToLibrary();
+}
+
+
+function addBookToLibrary() {
+    const bookDisplay = document.getElementById('book-display')
+    bookDisplay.innerHTML = '';
+
+    for (let i = 0; i < myBooks.length; i++) {
+        const book = myBooks[i];
+
+        const bookDiv = document.createElement('div')
+        bookDiv.classList.add('book')
+
+        const title = document.createElement('p')
+        title.textContent = `Title: ${book.title}`
+
+        const author = document.createElement('p')
+        author.textContent = `Author: ${book.author}`
+
+        const pages = document.createElement('p')
+        pages.textContent = `Pages: ${book.pages}`
+
+        const read = document.createElement('p')
+        read.textContent = `Read: ${book.read ? 'Yes' : 'No'}`
+
+        bookDiv.appendChild(title);
+        bookDiv.appendChild(author);
+        bookDiv.appendChild(pages);
+        bookDiv.appendChild(read);
+        bookDisplay.appendChild(bookDiv);
     }
 }
 
-function addBookToLibrary() {
-
-
+window.onload = function () {
+    const newBookBtn = document.getElementById('newBookBtn')
+    newBookBtn.addEventListener('click', addBook)
 }
-
-const theHobbit = new Book('The Hobbit ', 'By J.R.R Tolkien ', '295 pages ', 'Not read yet')
-//return theHobbit.info()
-
