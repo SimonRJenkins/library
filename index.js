9d0b00be59d62adf3659961d481a72ce6382bda4
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-07-06 18:05:33 +0100

    Update index.html, styles.css & index.js
    Added a paragraph (book-display) to display books from the array of books in index.js
    Added a new book button and added styling
    Added a books header for the array of books to go below
    Added function to new book button to give a prompt for a the book's information when clicked and adds it to the array

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+bookDisplay = document.getElementById("book-display")
+newBook = document.getElementById("new-book")
+
 let myBooks = [];
+bookDisplay.innerHTML = [myBooks];
+
+
+newBook.addEventListener("click", function () {
+    var title = prompt("What book would you like to add?")
+    var author = prompt("Who is the author of this book?")
+    var pages = prompt("How many pages does this book have?")
+    var read = prompt("Have you read this book?")
+    myBooks.push(title, author, pages + " pages", read);
+    bookDisplay.innerHTML = [myBooks];
+})
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,7 +26,9 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
 
+
 }
 
 const theHobbit = new Book('The Hobbit ', 'By J.R.R Tolkien ', '295 pages ', 'Not read yet')
-return theHobbit.info()
+//return theHobbit.info()
+
